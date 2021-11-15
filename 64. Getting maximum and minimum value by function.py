65c20ff42c177114fed4cf843aa96594d0b1e5f4
Sarmistha Pal <sarmistha15-9306@diu.edu.bd>
2021-11-15 20:34:00 +0600

    Add files via upload

---
+++ 64. Getting maximum and minimum value by function.py
@@ -0,0 +1,17 @@
+def maximum(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+
+result=maximum(4,8)
+print("Maximum value is:", result)
+
+def minimum(a,b):
+    if a<b:
+        return a
+    else:
+        return b
+
+result=minimum(4,8)
+print("Minimum value is:", result)
