78636f43a204089c244de770fe743cc254337216
Sarmistha Pal <sarmistha15-9306@diu.edu.bd>
2021-11-15 20:35:04 +0600

    Add files via upload

---
+++ 67. Lambda Function.py
@@ -0,0 +1,8 @@
+# Lambda function is a function, which is without name
+# not powerful as named function
+# can work with single expression
+# Lambda parameter : expression
+
+print((lambda a,b: a+b)(4,5))
+l=(lambda a,b : a*a + 2*a*b + b*b)(2,3)
+print(l)
